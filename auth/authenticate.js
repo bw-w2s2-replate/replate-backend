b9d8c9c46f383147ff14f20005c29fe3d9c2d8ae
umbur <you@example.com>
2019-03-19 09:41:05 -0600

    created authenticate file to generate token after authhorization

---
+++ auth/authenticate.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+
+const jwKey = process.env.JWT_SECRET || 'add a .env file to root of project with the JWT_SECRET variable';
+
+module.exports = {
+    authenticate,
+    generateToken
+}
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+    const options = {
+        expiresIn: 'id',
+    }
+    return jwt.sign(payload, jwtKey, options)
+}
+
+function authenticate(req, res, next) {
+    const token = req.get('Authorization');
+  
+    if (token) {
+      jwt.verify(token, jwtKey, (err, decoded) => {
+        if (err) return res.status(401).json(err);
+  
+        req.decoded = decoded;
+  
+        next();
+      });
+    } else {
+      return res.status(401).json({
+        error: 'No token provided, must be set on the Authorization Header',
+      });
+    }
+  }
