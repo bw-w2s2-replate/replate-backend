f02a425909332299ac96a187b836eac236ddf10d
umbur <you@example.com>
2019-03-19 09:51:22 -0600

    created config folder for routes; created dbConfig file; installed bcryptjs dependenciy

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+const configureRoutes = require('../config/routes.js');
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Server is working!' })
