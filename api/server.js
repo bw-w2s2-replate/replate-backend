a3d9211d91f3a4707df53cc547ff11e3f72bee10
umbur <you@example.com>
2019-03-19 21:03:39 -0600

    created GET endpoint to get by id from requests table

--- api/server.js
+++ api/server.js
@@ -98,6 +98,21 @@ server.delete('/api/users/:id', async (req, res) => {
     }
 })
 
+server.get('/api/requests/:id', async (req, res) => {
+    try { 
+        const { id } = req.params
+        const data = await requests.findById(id)
+        //console.log(data)
+        if(data.length > 0) {
+            res.status(200).json(data)
+        } else {
+            res.status(400).json({ error: `Request with that id doesn't exists` })
+        }
+    } catch(err) {
+        res.status(500).json({ error: 'Database error' })
+    }
+})
+
 
 
 configureRoutes(server);
